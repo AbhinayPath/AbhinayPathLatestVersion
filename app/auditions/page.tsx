00d82b865a1d339510d8dca098f158723f5e2d17
saurabhsep99 <saurabhsep99@outlook.com>
2025-05-28 17:59:08 +0530

    added env variable prefix

--- app/auditions/page.tsx
+++ app/auditions/page.tsx
@@ -7,7 +7,7 @@ export const revalidate = 60;
 async function getAuditions() {
   try {
     // Use relative path instead of absolute with NEXT_PUBLIC_BASE_URL
-    const res = await fetch("http://localhost:3000/api/auditions", {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/auditions`, {
       next: { revalidate: 60 },
     });
 
