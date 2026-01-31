0d4f2aee1fd5aff78aceb95525e9f744db1c34d0
sreehari-zaigo <110631088+sreehari-k-19@users.noreply.github.com>
2026-01-31 12:07:42 +0530

    feat: Implement Supabase authentication callback handling and a new responsive Navbar component with user authentication and profile management.

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -7,10 +7,13 @@ import { cookies } from 'next/headers'
 export async function GET(request: NextRequest) {
     const { searchParams } = new URL(request.url)
     const token_hash = searchParams.get('token_hash')
+    const code = searchParams.get('code')
     const type = searchParams.get('type') as EmailOtpType | null
-
+    console.log("token_hash", token_hash)
+    console.log("code", code)
+    console.log("type", type)
     if (token_hash && type) {
-        const supabase = createRouteHandlerClient({ cookies });
+        const supabase = await createRouteHandlerClient({ cookies });
         const { error } = await supabase.auth.verifyOtp({ type, token_hash })
         if (!error) {
             return NextResponse.redirect(new URL('/', request.url))
